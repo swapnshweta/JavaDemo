bbf813cd9111649bc3925c3767a205c9723799bf
swapnshweta <pawar.swapnil59@gmail.com>
2026-02-03 15:23:36 +0530

    welcome

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,5 +2,6 @@ public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
         System.out.println("Build triggered using GitHub Webhook 🚀");
+        System.out.println("Welcome");
     }
 }
